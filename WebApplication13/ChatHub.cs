b2e1c42eeb93933e389c5ddb724c9ed9ad6e6584
Grekov <grrekov@yandex.ru>
2020-10-06 01:54:34 +0300

    Add project files.

---
+++ WebApplication13/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication13
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
